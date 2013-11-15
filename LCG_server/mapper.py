7fdf8fe180a371ba30a05e53a637620f5218d4b7
modrzew <modrzew@gmail.com>
2013-11-15 23:12:17 +0100

    Generating gold & players' starting positions

--- LCG_server/mapper.py
+++ LCG_server/mapper.py
@@ -1,8 +1,11 @@
+import random
 
 from models import Trujkont
 
 
 class Mapper(object):
+    GOLD = 0.1  # how many % of trujkonts get gold
+
     def __init__(self, ovrs, rows, cols):
         self.ovrs = ovrs
         self.rows = int(rows)
@@ -13,6 +16,21 @@ class Mapper(object):
             [Trujkont(row, col) for col in range(self.cols)]
             for row in range(self.rows)
         ]
+        for _ in range(int(self.rows*self.cols*self.GOLD)):
+            trujkont = None
+            while not Trujkont:
+                row = random.choice(arr)
+                trujkont = random.choice(row)
+            trujkont.gold = random.randint(10, 100)
+        for p in self.ovrs.players:
+            trujkont = None
+            while not Trujkont:
+                row = random.choice(arr)
+                trujkont = random.choice(row)
+                if trujkont and (trujkont.gold or trujkont.owner):
+                    trujkont = None
+            trujkont.owner = p
+            trujkont.building = 'townhall'
         self.map = arr
 
     def to_dict(self):
