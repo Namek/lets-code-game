580b624dfcf5d1cad1c684d532bf95f3e0b7e736
Porke <porke90@gmail.com>
2013-11-16 04:41:13 +0100

    Added endgame handling.

--- LCG/src/com/letscode/lcg/screens/GameResultScreen.java
+++ LCG/src/com/letscode/lcg/screens/GameResultScreen.java
@@ -1,12 +1,21 @@
 package com.letscode.lcg.screens;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.letscode.lcg.Context;
 import com.letscode.ui.BaseScreen;
 
 public class GameResultScreen extends BaseScreen {
 
-	public GameResultScreen(Context context) {
+	public GameResultScreen(Context context, String winner) {
 		super(context.app);
+	    
+	    String text = context.getPlayerNickname().equals(winner) ? "You have won!" : "Player " + winner + " has won."; 
+	    Label label = new Label(text, app.skin); 
+	    label.setAlignment(Align.center); 
+	    mainTable.add(label).expand().fill(); 
+	    
+	    mainTable.layout(); 
 	}
 
 	@Override
