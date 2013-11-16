e28b923e12ac3f8431e6e14963a27c7734d4d159
Porke <porke90@gmail.com>
2013-11-16 05:07:56 +0100

    Added particle system on endgame.

--- LCG/src/com/letscode/lcg/screens/GameResultScreen.java
+++ LCG/src/com/letscode/lcg/screens/GameResultScreen.java
@@ -1,12 +1,17 @@
 package com.letscode.lcg.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.letscode.lcg.Context;
+import com.letscode.lcg.actor.ParticleSystem;
 import com.letscode.ui.BaseScreen;
 
 public class GameResultScreen extends BaseScreen {
 
+	private ParticleSystem leftSystem;
+	private ParticleSystem rightSystem;
+	
 	public GameResultScreen(Context context, String winner) {
 		super(context.app);
 	    
@@ -15,10 +20,26 @@ public class GameResultScreen extends BaseScreen {
 	    label.setAlignment(Align.center); 
 	    mainTable.add(label).expand().fill(); 
 	    
+	    leftSystem = new ParticleSystem("winner.ps");
+	    rightSystem = new ParticleSystem("winner.ps");
+	    
+	    leftSystem.setPosition(Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight() / 2);
+	    rightSystem.setPosition(Gdx.graphics.getWidth() / 3 * 2, Gdx.graphics.getHeight() / 2);
+	    addActor(leftSystem);
+	    addActor(rightSystem);
+	    
 	    mainTable.layout(); 
 	}
 
 	@Override
+	public void act(float delta) {
+		super.act(delta);
+		leftSystem.act(delta);
+		rightSystem.act(delta);
+	};
+	
+	@Override
 	public void onBackPress() {	}
 
 }
+
